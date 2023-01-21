550209cfbf25027cc44ac2e654cb21502d8f0e2d
professional-coding-projects <106799068+professional-coding-projects@users.noreply.github.com>
2023-01-21 16:48:28 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,29 @@
+class Box
+  {
+    constructor(x,y,w,h,vx,vy)
+    {
+      this.x =x;
+      this.y =y;
+      this.w =w;
+      this.h = h;
+      this.vx = vx;
+      this.vy= vy;
+    }
+    
+    show()
+    {
+      rect(this.x,this.y,this.w,this.h)
+    }
+    
+    moveup(){
+      this.y=this.y-this.vy;
+    }
+    move()
+    {
+      this.x = this.x+this.vx;
+
+    }
+
+  }
+
+  
